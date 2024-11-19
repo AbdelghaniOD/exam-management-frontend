71160d69333e087f4a23d228463180e998cf85e0
abdelghaniOD <ouladdahman.abdelghani@gmail.com>
2024-11-19 21:34:24 +0100

    done...

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure this is scanning all React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
